638ceacc70bdedbb5c8656445d4f9ff5377c93e5
nkzren <renan.nakazawa0@gmail.com>
2021-09-08 19:57:45 -0300

    commit inicial

---
+++ src/test/java/br/usp/each/pand/NativeGreetingResourceIT.java
@@ -0,0 +1,9 @@
+package br.usp.each.pand;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeGreetingResourceIT extends GreetingResourceTest {
+
+    // Execute the same tests but in native mode.
+}
