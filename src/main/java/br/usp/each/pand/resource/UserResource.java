5fffae9e9cee7a882dabe2e1ef6cc5d7c861cf81
nkzren <renan.nakazawa0@gmail.com>
2021-11-03 21:32:21 -0300

    adiciona login

--- src/main/java/br/usp/each/pand/resource/UserResource.java
+++ src/main/java/br/usp/each/pand/resource/UserResource.java
@@ -2,12 +2,14 @@ package br.usp.each.pand.resource;
 
 import br.usp.each.pand.model.User;
 import br.usp.each.pand.service.UserService;
+import org.jboss.resteasy.annotations.Form;
 
 import javax.inject.Inject;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.net.URI;
+import java.util.Optional;
 
 @Path("/user")
 @Produces(MediaType.APPLICATION_JSON)
@@ -32,5 +34,28 @@ public class UserResource {
         return Response.status(201).build();
     }
 
+    @GET
+    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+    public Response login(@FormParam("username") String username, @FormParam("password") String password) {
+        try {
+            if (null == username || null == password || username.isEmpty() || password.isEmpty()) {
+                return Response.status(400)
+                        .entity("username and password invalid")
+                        .build();
+            }
+
+            Optional<User> maybeUser = userService.login(username, password);
+            if (maybeUser.isPresent()) {
+                return Response.status(200).entity(maybeUser.get()).build();
+            } else {
+                return Response.status(404).entity("user not found").build();
+            }
+
+        } catch (Exception e) {
+            return Response.status(500)
+                    .entity("Unexpected error")
+                    .build();
+        }
+    }
 
 }
